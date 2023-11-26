ae7933167bd36fcae127824de7323d367db24aaf
Fabricio Altamirano <fabricio.altamirano>
2023-11-26 17:20:52 +0100

    move project

--- project/main/app_main.c
+++ project/main/app_main.c
@@ -160,6 +160,11 @@ void app_main(void)
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
     ESP_ERROR_CHECK(example_connect());
 
     mqtt_app_start();
