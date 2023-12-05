7bf97810e6c40c98cc035231f1047fabbf9a0803
Fabricio Altamirano <fabricio.altamirano>
2023-12-05 17:11:04 +0100

    version thinger io

--- ard/climastic/climastic.ino
+++ ard/climastic/climastic.ino
@@ -1,42 +1,75 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <Ticker.h>
 
 #include <ClimaStick.h>
 
-#define USERNAME "fabricio"
-#define DEVICE_ID "Bicimap"
-#define DEVICE_CREDENTIAL "Zah9jS#f9m$&"
+#define USERNAME "javigrz"
+#define DEVICE_ID "thingerioMQTT"
+#define DEVICE_CREDENTIAL "XSTld@si%RN0RG!B"
 
-#define SSID "SBC"
-#define SSID_PASSWORD "SBCwifi$"
+// Instancia a la clase Ticker
+Ticker ticker;
 
+// Pin LED azul
+byte pinLed = D4;
 
 ClimaStick thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
- // configure board wifi
-  thing.add_wifi(SSID, SSID_PASSWORD);
-  // initialize board sensors
-  thing.init_sensors();
-  // define resources for all features
-  thing.init_resources();
+void parpadeoLed(){
+ // Cambiar de estado el LED
+byte estado = digitalRead(pinLed);
+digitalWrite(pinLed, !estado);
+}
+
+void setup()
+{
+Serial.begin(115200);
+
+ // Modo del pin
+pinMode(pinLed, OUTPUT);
+
+ // Empezamos el temporizador que hará parpadear el LED
+ticker.attach(0.2, parpadeoLed);
+
+ // Creamos una instancia de la clase WiFiManager
+WiFiManager wifiManager;
+
+ // Descomentar para resetear configuración
+ //wifiManager.resetSettings();
+
+ // Cremos AP y portal cautivo y comprobamos si
+ // se establece la conexión
+if(!wifiManager.autoConnect("ESP8266Temp")){
+Serial.println("Fallo en la conexión (timeout)");
+ESP.reset();
+delay(1000);
+}
+
+Serial.println("Ya estás conectado");
+
+ // Eliminamos el temporizador
+ticker.detach();
+
+ // Apagamos el LED
+digitalWrite(pinLed, HIGH);
+
+thing.init_sensors();
+ // define the "environment" resource
+thing.init_environment_resource();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-   thing.handle();
-
-    // write to bucket BucketId
-    thing.write_bucket("storage_climastick", "environment");
-
-     Serial.println("Ready Gyroscope");
-  
-    float temperature = thing.get_temperature();
-    thing.get_acceleration();
-    Serial.println(temperature);
-
-
-    // sleep the device 60 seconds
-   thing.sleep(60);
-
+Serial.println("Inicio");
+thing.handle();
+Serial.println("handle");
+ // write to bucket BucketId
+thing.write_bucket("data", "environment");
+Serial.println("bucket");
+ // sleep the device 60 seconds
+thing.sleep(60);
 }
+
+
